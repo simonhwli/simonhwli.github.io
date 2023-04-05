26c687e07f0f11de9e099b151e0ca2a9eecfd965
Li <Simon.Li@aremedia.com.au>
2023-04-05 11:16:48 +1000

    work

--- data.js
+++ data.js
@@ -1,15 +1,21 @@
+const { updateDynamicPlaylistMedia } = require("./playlist.js");
+
 export default function getJWPlayerPlaylistData(playlist_id, jwplayer_api_key) {
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${jwplayer_api_key}`
-    }
+      accept: "application/json",
+      Authorization: `Bearer ${jwplayer_api_key}`,
+    },
   };
   const endpoint = `https://api.jwplayer.com/v2/sites/GRJTlii1/playlists/${playlist_id}/`;
-  
+
+  updateDynamicPlaylistMedia("bIs9UNqY", "VZwxV83odDrN3U0SnOq6QmInU1c1Q00yRkxXRFZYVnpKR2QzRlpRa0Z4TW1GblJuZDUn")
+    .then((response) => console.log("update response: ", response))
+    .catch((error) => console.error(error));
+
   return fetch(endpoint, options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
 }
